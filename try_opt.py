bbaa24092961f5cc34e9465de09b07465c8f091d
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-07-06 00:29:24 -0400

    minor

--- try_opt.py
+++ try_opt.py
@@ -6,10 +6,11 @@ def main():
     # https://huggingface.co/facebook/opt-66b
     # Notes: try without torch_dtype=torch.float16
 
-    model_name = "facebook/opt-2.7b" # facebook/opt-6.7b, facebook/opt-13b, facebook/opt-30b, facebook/opt-66b
+    # facebook/opt-2.7b, facebook/opt-6.7b, facebook/opt-13b, facebook/opt-30b, facebook/opt-66b
+    model_name = "facebook/opt-30b"
 
     model = AutoModelForCausalLM.from_pretrained(
-        model_name, device_map="auto", torch_dtype=torch.float16
+        model_name, revision="main", device_map="auto", torch_dtype=torch.float16
     ).cuda()
 
     # the fast tokenizer currently does not work correctly
