746358a9461cbd104555b2768079a4ae187be4c3
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-07-17 15:11:40 -0400

    minor

--- download_models.py
+++ download_models.py
@@ -5,10 +5,11 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 def main():
-    model_shortnames in ["gptj", "opt", "neox20b", "t0pp"]
+    model_shortnames = ["gptj", "opt", "neox20b", "t0pp"]
     for model_shortname in model_shortnames:
         print(f"Downloading and caching {model_shortname}")
         os.environ["MODEL_NAME"] = model_shortname
+        from serve_models.main import get_model_and_tokenizer # this has to be here.
         get_model_and_tokenizer()
 
 if __name__ == '__main__':
