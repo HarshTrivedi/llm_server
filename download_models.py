6dd3b759f374ed0f4849fc944a513b68c2db295e
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-07-17 20:05:57 -0400

    update download_models.py

--- download_models.py
+++ download_models.py
@@ -1,16 +1,42 @@
 import os
-os.environ['TRANSFORMERS_CACHE'] = 'hf_models_cache' # needs to be called before importing transformers
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from importlib import reload
 
 def main():
-    model_shortnames = ["gptj", "opt", "neox20b", "t0pp"]
-    for model_shortname in model_shortnames:
-        print(f"Downloading and caching {model_shortname}")
-        os.environ["MODEL_NAME"] = model_shortname
-        from serve_models.main import get_model_and_tokenizer # this has to be here.
-        get_model_and_tokenizer()
+
+    # Download gpt-j-6B
+    reload(transformers)
+    organization = "EleutherAI"
+    short_model_name = "gpt-j-6B"
+    os.environ['TRANSFORMERS_CACHE'] = os.path.join("hf_models_cache", short_model_name)
+    model_name = os.path.join(organization, short_model_name)
+    transformers.AutoModelForCausalLM.from_pretrained(model_name, revision="sharded")
+    transformers.AutoTokenizer.from_pretrained(model_name)
+
+    # Download opt-66b
+    reload(transformers)
+    organization = "facebook"
+    short_model_name = "opt-66b"
+    os.environ['TRANSFORMERS_CACHE'] = os.path.join("hf_models_cache", short_model_name)
+    model_name = os.path.join(organization, short_model_name)
+    transformers.AutoModelForCausalLM.from_pretrained(model_name, revision="main")
+    transformers.AutoTokenizer.from_pretrained(model_name, use_fast=False) # fast doesn't work here.
+
+    # Download gpt-neox-20b
+    reload(transformers)
+    organization = "EleutherAI"
+    short_model_name = "gpt-neox-20b"
+    os.environ['TRANSFORMERS_CACHE'] = os.path.join("hf_models_cache", short_model_name)
+    model_name = os.path.join(organization, short_model_name)
+    transformers.AutoModelForCausalLM.from_pretrained(model_name, revision="main")
+    transformers.AutoTokenizer.from_pretrained(model_name)
+
+    # Download T0pp
+    reload(transformers)
+    organization = "bigscience"
+    short_model_name = "T0pp"
+    transformers.AutoModelForSeq2SeqLM.from_pretrained(model_name, revision="sharded")
+    transformers.AutoTokenizer.from_pretrained(model_name)
 
 if __name__ == '__main__':
     main()
