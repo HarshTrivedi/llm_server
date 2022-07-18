e040cd39bc8f4a7962db7ac01f2d8d5f19ba0d74
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-07-17 20:11:29 -0400

    download opt the last.

--- download_models.py
+++ download_models.py
@@ -13,15 +13,6 @@ def main():
     transformers.AutoModelForCausalLM.from_pretrained(model_name, revision="sharded")
     transformers.AutoTokenizer.from_pretrained(model_name)
 
-    # Download opt-66b
-    reload(transformers)
-    organization = "facebook"
-    short_model_name = "opt-66b"
-    os.environ['TRANSFORMERS_CACHE'] = os.path.join("hf_models_cache", short_model_name)
-    model_name = os.path.join(organization, short_model_name)
-    transformers.AutoModelForCausalLM.from_pretrained(model_name, revision="main")
-    transformers.AutoTokenizer.from_pretrained(model_name, use_fast=False) # fast doesn't work here.
-
     # Download gpt-neox-20b
     reload(transformers)
     organization = "EleutherAI"
@@ -37,6 +28,15 @@ def main():
     short_model_name = "T0pp"
     transformers.AutoModelForSeq2SeqLM.from_pretrained(model_name, revision="sharded")
     transformers.AutoTokenizer.from_pretrained(model_name)
+
+    # Download opt-66b
+    reload(transformers)
+    organization = "facebook"
+    short_model_name = "opt-66b"
+    os.environ['TRANSFORMERS_CACHE'] = os.path.join("hf_models_cache", short_model_name)
+    model_name = os.path.join(organization, short_model_name)
+    transformers.AutoModelForCausalLM.from_pretrained(model_name, revision="main")
+    transformers.AutoTokenizer.from_pretrained(model_name, use_fast=False) # fast doesn't work here.
 
 if __name__ == '__main__':
     main()
