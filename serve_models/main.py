60117cdabcf8ed727fa3650d19facec0f4f25c53
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-11-14 19:18:25 -0500

    minor

--- serve_models/main.py
+++ serve_models/main.py
@@ -71,7 +71,7 @@ def get_model_and_tokenizer():
 
         if "xxl" in model_shortname:
             model = T5ForConditionalGeneration.from_pretrained(
-                model_name, revision="main", device_map="auto", load_in_8bit=True
+                model_name, revision="main", device_map="auto", torch_dtype=torch.float16
             )
         else:
             model = T5ForConditionalGeneration.from_pretrained(
