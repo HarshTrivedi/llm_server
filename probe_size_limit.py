a637a095d2cbce2bfbed9f9cbfd51da69f59e947
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-11-22 22:12:55 -0500

    minor

--- probe_size_limit.py
+++ probe_size_limit.py
@@ -4,10 +4,10 @@ import json
 def main():
 
     # This is just an example. Change as necessary.
-    host = "http://general-cirrascale-15.reviz.ai2.in"
-    port = 49192
+    host = "http://allennlp-cirrascale-01.reviz.ai2.in"
+    port = 49153
 
-    probe_size_limits = (300, 4000)
+    probe_size_limits = (300, 5000)
     probe_size_increments = 100
 
     probe_size = probe_size_limits[0]
@@ -19,7 +19,7 @@ def main():
 
         print(f"Probing size {probe_size}")
         prompt = " ".join((["a"]*probe_size))
-        params = {"prompt": prompt, "min_length": 1, "max_length": probe_size + 1}
+        params = {"prompt": prompt, "min_length": 1, "max_length": 10}
         response = requests.get(host + ":" + str(port) + "/generate", params=params)
         if response.ok:
             model_name = response.json()["model_name"]
