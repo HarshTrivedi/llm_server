3444d5f47cef0bd63e808cb03f3cbc58483ee76e
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-11-22 19:54:15 -0500

    minor

--- probe_size_limit.py
+++ probe_size_limit.py
@@ -4,26 +4,29 @@ import json
 def main():
 
     # This is just an example. Change as necessary.
-    host = "http://general-cirrascale-16.reviz.ai2.in"
-    port = 49173
+    host = "http://general-cirrascale-15.reviz.ai2.in"
+    port = 49192
 
-    probe_size_limits = (500, 2500)
+    probe_size_limits = (300, 4000)
     probe_size_increments = 100
 
     probe_size = probe_size_limits[0]
     while True:
-        print(f"Probing size {probe_size}")
 
         probe_size += probe_size_increments
         if probe_size>= probe_size_limits[1]:
             break
 
+        print(f"Probing size {probe_size}")
         prompt = " ".join((["a"]*probe_size))
-        params = {"prompt": prompt, "min_length": 1, "max_length": 1}
+        params = {"prompt": prompt, "min_length": 1, "max_length": probe_size + 1}
         response = requests.get(host + ":" + str(port) + "/generate", params=params)
         if response.ok:
             model_name = response.json()["model_name"]
-            print(f"Successful ({model_name})")
+            print(response.json()["generated_texts"][0])
+            num_generated_tokens = response.json()["generated_num_tokens"]
+            run_time_in_seconds = response.json()["run_time_in_seconds"]
+            print(f"Successful ({model_name}, {num_generated_tokens}, {run_time_in_seconds})")
         else:
             print("Failed")
 
