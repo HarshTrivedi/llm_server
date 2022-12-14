de6aae77770b469f32b966c9cdaffe8971742745
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-12-13 21:08:39 -0500

    minor

--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
-TRANSFORMERS_CACHE = "/net/nfs.cirrascale/aristo/llm_server/.hf_cache"
+# TRANSFORMERS_CACHE = "/net/nfs.cirrascale/aristo/llm_server/.hf_cache" # cirrascale
+TRANSFORMERS_CACHE = "/net/nfs/aristo/llm_server/" # elanding
